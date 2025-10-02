87c654181af2b46c095a6909ff159a5215bfdd09
Juang Maulana Taruna Putra <117959461+juangmaulana@users.noreply.github.com>
2025-10-02 16:14:10 +0700

    Update posts.service.ts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -5,5 +5,35 @@ import { PrismaService } from '../prisma.service';
 export class PostsService {
   constructor(private prisma: PrismaService) {}
 
-  // TODO: Implement
+     // TODO: Implement
+    async create(data: any) {
+      return this.prisma.post.create({ data });
+    }
+
+    async findAll() {
+      return this.prisma.post.findMany({
+        orderBy: { createdAt: 'desc' },
+        include: { replies: true, replyTo: true },
+      });
+    }
+
+    async findOne(id: string) {
+      return this.prisma.post.findUnique({
+        where: { id },
+        include: { replies: true, replyTo: true },
+      });
+    }
+
+    async update(id: string, data: any) {
+      return this.prisma.post.update({
+        where: { id },
+        data,
+      });
+    }
+
+    async remove(id: string) {
+      return this.prisma.post.delete({
+        where: { id },
+      });
+    }
 }
